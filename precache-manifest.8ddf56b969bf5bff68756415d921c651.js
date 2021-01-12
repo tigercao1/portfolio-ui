ea7a33586771025e7fbcb659723698be0adb90e3
Tiger Cao <tigercao1@gmail.com>
2021-01-12 15:58:33 -0500

    Updates

---
+++ precache-manifest.8ddf56b969bf5bff68756415d921c651.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9956203fdc7d708c25f2cf2ba68dd401",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd76ca2c903456e22ca5",
+    "url": "/static/css/main.81f70aff.chunk.css"
+  },
+  {
+    "revision": "489d054ad317091e7a97",
+    "url": "/static/js/2.8d395192.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/static/js/2.8d395192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd76ca2c903456e22ca5",
+    "url": "/static/js/main.2fe5997a.chunk.js"
+  },
+  {
+    "revision": "7b9c6b5b3eaf095adc1a",
+    "url": "/static/js/runtime-main.5da463a6.js"
+  },
+  {
+    "revision": "278171beffefb0caec3020ffb6503db4",
+    "url": "/static/media/Solace-logo-green.278171be.png"
+  },
+  {
+    "revision": "7f57dce86421d8e8d094f24212c6bb06",
+    "url": "/static/media/carleton_logo.7f57dce8.png"
+  },
+  {
+    "revision": "0450316ae00bf09e09610b7e6c7525a2",
+    "url": "/static/media/my_photo.0450316a.png"
+  },
+  {
+    "revision": "c221fbc14889e9ec383a2b8f89065428",
+    "url": "/static/media/tiger-icon-png-29.c221fbc1.jpg"
+  },
+  {
+    "revision": "367fbdeebb31e9b2c43fed5b903e7c31",
+    "url": "/static/media/trendmicro.367fbdee.png"
+  }
+]);
